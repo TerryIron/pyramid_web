f17360e6bec63cfb4dfd81f17f0af93f863e5edc
xichijun <xicj@wiseonline.com.cn>
2018-10-19 11:28:01 +0800

    更新脚本

--- run_ini.sh
+++ run_ini.sh
@@ -10,6 +10,13 @@ CONFIG=$1
     }
 }
 
+for i in $(find | grep setup.py); do 
+    j=$(dirname $i); 
+    [ "$j" != "." ] && {
+        cd $j && python setup.py bdist_egg && cd -
+    }
+done
+
 PWD=$(pwd)
 export PYTHONPATH=$PWD
 python $(which pserve) $CONFIG
