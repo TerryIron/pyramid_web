5330d0586a521bfa4afa4a721e1fb17b5ca1cdf5
xichijun <xicj@wiseonline.com.cn>
2018-11-01 15:43:54 +0800

    更新脚本

--- run_ini.sh
+++ run_ini.sh
@@ -3,14 +3,14 @@
 CONFIG=$1
 
 [ "$CONFIG"  == "" ] && {
-    CONFIG="./production.ini"
+        CONFIG="$(pwd)/production.ini"
 } || { 
     [ "$CONFIG" == "-d" ] && {
-        CONFIG="./development.ini"
+        CONFIG="$(pwd)/development.ini"
     }
 }
 
-for i in $(find | grep setup.py); do 
+for i in $(find | grep setup.py$); do 
     j=$(dirname $i); 
     [ "$j" != "." ] && {
         cd $j && python setup.py bdist_egg && cd -
@@ -19,4 +19,5 @@ done
 
 PWD=$(pwd)
 export PYTHONPATH=$PWD
+echo python $(which pserve) $CONFIG
 python $(which pserve) $CONFIG
