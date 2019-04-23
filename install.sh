d55cebf9ef0127d1d3e01587fffe59e823738ffc
xichijun <xicj@wiseonline.com.cn>
2019-04-23 11:53:11 +0800

    更新脚本

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-apt-get install libmysqlclient-dev python-pip virtualenv
+apt-get install libmysqlclient-dev python-pip virtualenv*
+
+virtualenv env --no-site-packages
+source env/bin/activate && pip install setuptools==3.4.4
+source env/bin/activate && pip install -r requirements.txt
