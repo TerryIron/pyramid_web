e70cbf725f5d852115dc13d672b83759238e920b
xichijun <xicj@wiseonline.com.cn>
2019-04-23 11:53:11 +0800

    更新脚本

---
+++ installs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+PWD=$(pwd)
+apt-get install build-essential python-dev python-pip libmysqlclient-dev virtualenv*
+virtualenv env --no-site-packages
+TORNADO=$(cat requirements.txt | grep "tornado=\|tornado$")
+[ "$TORNADO" == "" ] && {
+    UWSGI_EMBED_PLUGINS="greenlet"
+} || {
+    UWSGI_EMBED_PLUGINS="tornado,greenlet"
+}
+source env/bin/activate && pip install greenlet 
+cp -rf $PWD/env/include/site/python2.7/greenlet.h /usr/local/include 
+mkdir /usr/local/include/greenlet
+cp -rf $PWD/env/include/site/python2.7/greenlet.h /usr/local/include/greenlet
+source env/bin/activate && UWSGI_EMBED_PLUGINS=$UWSGI_EMBED_PLUGINS pip install $TORNADO greenlet uwsgi==2.0.15
+source env/bin/activate && pip install setuptools==3.4.4
+source env/bin/activate && pip install -r requirements.txt
+
