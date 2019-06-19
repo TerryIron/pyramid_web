2c620ac26078016cd257e3f28fd8301faa06492f
xichijun <xicj@wiseonline.com.cn>
2019-06-19 17:06:01 +0800

    更新脚本

--- stop.sh
+++ stop.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env python
-#coding:utf-8
+# coding:utf-8
 import commands
 import os
 import sys
 
-def kill_process(pid):
-    _str = commands.getoutput("netstat -lnpte|grep {}".format(pid) + "|awk '{print $NF}'")
-    p_pp_li = _str.split('/')
-    print p_pp_li
 
-    os.system('kill -9 {} 2>/dev/null'.format(p_pp_li[0]))
+def kill_process(port):
+    port_info = commands.getoutput('netstat -tanp 2>&1 | grep tcp | grep LISTEN | grep ":{}"'.format(port))
+    if port_info:
+        port_pid = commands.getoutput('echo {} '.format(port_info) + " | awk '{print $7}'").split('\n')
+        port_pid = [i.split('/')[0] for i in port_pid]
+        os.system('kill -9 {} 2>/dev/null'.format(' '.join(port_pid)))
+        return port_pid
+    return []
 
 
 if __name__ == '__main__':
-    pid = sys.argv[1]
-    kill_process(pid)
-
+    port = sys.argv[1]
+    while True:
+        ret = kill_process(port)
+        if not ret:
+            break
